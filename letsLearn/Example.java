08bb1b081a66ce3b09d8f3aa8c0849858e634d9d
sabiinsearch <sabiinsearch@gmail.com>
2021-03-29 22:07:41 +0530

    update

--- letsLearn/Example.java
+++ letsLearn/Example.java
@@ -1,1 +1,10 @@
-Learnng various aspects like
+/*
+This is a simple java programme.
+Call this file "Example.java"
+*/
+class Example {
+  // Programme begins with call to main()
+  public static void main(String args[]) {
+    System.out.println("This is a simple Java Programme");
+  }
+}
