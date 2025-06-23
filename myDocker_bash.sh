ede358bee8143215337b9b86f0d67f8a05820501
Sarbjeet Singh <sabiinsearch@gmail.com>
2025-06-23 17:11:33 +0530

    Update myDocker_bash.sh

--- myDocker_bash.sh
+++ myDocker_bash.sh
@@ -12,7 +12,10 @@ else
   echo "Network '$network_name' created."
 fi
 ## Create SQL Server in same network
-docker run --name mysql-server --network myDocker_net_1 -v mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d -p 3306:3306 mysql:latest
+if docker ps -a | grep -q "mysql-server";then
+   echo "mysql-server already exist"
+else
+   docker run ---name mysql-server --network myDocker_net_1 -v mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d -p 3306:3306 mysql:latest
 
 ## Create SQL client in same network
 docker run -it --rm --network myDocker_net_1 mysql:8.0 mysql -h mysql-server -u root -p
